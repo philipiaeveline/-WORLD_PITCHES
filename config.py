55f9bc0cb3bead52a99f66d52fb3bc483c9ac03a
philipiaeveline <philipiaeveline13@gmail.com>
2020-12-07 22:38:25 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:eveline3434@localhost/pitches'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = True
+    
+
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:eveline3434@localhost/pitches'
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='evehope'
+export MAIL_USERNAME='philipiaeveline13@gmail.com'
+export MAIL_PASSWORD='eveline3434'
+
+python3.7 manage.py server
