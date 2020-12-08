33dba58715d3c31653aef71f215cd701bdb2900e
philipiaeveline <philipiaeveline13@gmail.com>
2020-12-08 11:33:16 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -5,9 +5,10 @@ from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
 
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
+
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
